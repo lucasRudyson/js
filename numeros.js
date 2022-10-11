0705c2ca573b18846625492839b2e73c7d38a671
Lucas-Rudyson <rudysonlucas@gmail.com>
2022-10-11 16:21:31 -0300

    repositorio de estudos em js

---
+++ numeros.js
@@ -0,0 +1,9 @@
+// numeros 
+
+const numero = 3;
+const numero2 = 1;
+const numero3 = '1';
+const nome = 'meu nome é lucas';
+
+console.log(numero2 == numero3)
+
---
+++ script.js
@@ -0,0 +1,9 @@
+// const nome = 'lucas'
+// const idade = 2022-2000;
+// const cidadeDeNascimento = 'Macau/RN';
+// const apresentacao = `meu nome é ${nome} minha idade é ${idade} e nasci em ${cidadeDeNascimento}`
+// console.log(apresentacao)
+function p (texto){
+    console.log(texto)
+
+}
